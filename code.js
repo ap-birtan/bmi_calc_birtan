74a193cb3268aab4bb3081ee17d2815c6a338acd
ap-birtan <158799925+ap-birtan@users.noreply.github.com>
2024-02-15 00:18:26 +0600

    Update code.js

--- code.js
+++ code.js
@@ -1,35 +1,62 @@
 
-
+let rn="r1";
 function rng(bmi)
 {
 	if(bmi<18.5)
 	{
+		rn="r2";
 		return "Under Weight!!";
+		
+
 
 	}
 	else if(bmi>=18.5&&bmi<=22.9)
 	{
+		rn="r3";
 		return "Normal, No Risk!";
+		
 	}
 	else if(bmi>=23&&bmi<=24.9)
 	{
+		rn="r4";
 		return "Over Weight!!"
+		
 	}
 	else if(bmi>=25&&bmi<=29.9)
 	{
+		rn="r5";
 		return "Pre-Obese!!";
+		
 	}
 
 	else if(bmi>30)
 	{
+		rn="r6";
 		return "Obese!!"
+		
 	}
 	else
 	{
 		return "please fill up correctly!"
 	}
 }
+function color(row)
+{
+   rn=document.getElementById("r2");
+   rn.style.border="none";
+   rn=document.getElementById("r3");
+   rn.style.border="none";
+   rn=document.getElementById("r4");
+   rn.style.border="none";
+   rn=document.getElementById("r5");
+   rn.style.border="none";
+   rn=document.getElementById("r6");
+   rn.style.border="none";
 
+   rn=document.getElementById(row);
+   rn.style.border="3px solid red";
+
+}
 function col(bmi)
 {
 	if(bmi<18.5)
@@ -66,13 +93,16 @@ let wt= parseInt(document.getElementById("wt").value);
 let dis=document.getElementById("dis");
 let bar=document.getElementById("bar");
 let tab=document.getElementById("tab");
+
 let res = wt/(ht*ht);
 let bmi=Math.round(res * 10) / 10;
    dis.innerHTML="Your BMI is: "+bmi+" <br> <p style=color:"+col(bmi)+ "> "+rng(bmi)+"<br>"+shiftt(bmi,wt,ht)+"</p>";
    bar.style.width=2*bmi+"%";
    bar.style.backgroundColor=col(bmi);
    tab.style.display="block";
+   color(rn);
    document.getElementById( 'dis' ).scrollIntoView();
+
 
 
 }
