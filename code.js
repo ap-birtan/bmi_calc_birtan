b1f0c78bdca5e1fe93977b85504c2f109c5e58c7
ap-birtan <158799925+ap-birtan@users.noreply.github.com>
2024-05-14 22:07:05 +0600

    Update code.js

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ function rng(bmi)
 	if(bmi<18.5)
 	{
 		rn="r2";
-		return "Under Weight!!";
+		return "Under Weight!! স্বাভাবিকের তুলনায় কম ওজন!!";
 		
 
 
@@ -13,31 +13,31 @@ function rng(bmi)
 	else if(bmi>=18.5&&bmi<=22.9)
 	{
 		rn="r3";
-		return "Normal, No Risk!";
+		return "Normal, No Risk! স্বাভাবিক ওজন, কোনো ঝুঁকি নেই।";
 		
 	}
 	else if(bmi>=23&&bmi<=24.9)
 	{
 		rn="r4";
-		return "Over Weight!!"
+		return "Over Weight!! স্বাভাবিকের তুলনায় একটু বেশি ওজন!!"
 		
 	}
 	else if(bmi>=25&&bmi<=29.9)
 	{
 		rn="r5";
-		return "Pre-Obese!!";
+		return "Pre-Obese!! স্বাভাবিকের তুলনায় অনেক বেশি ওজন!!";
 		
 	}
 
 	else if(bmi>30)
 	{
 		rn="r6";
-		return "Obese!!"
+		return "Obese!! অতিরিক্ত ওজন!!!"
 		
 	}
 	else
 	{
-		return "please fill up correctly!"
+		return "please fill up correctly! ঠিক ভাবে পূরণ করুন ।"
 	}
 }
 function color(row)
@@ -94,9 +94,10 @@ let dis=document.getElementById("dis");
 let bar=document.getElementById("bar");
 let tab=document.getElementById("tab");
 
+
 let res = wt/(ht*ht);
 let bmi=Math.round(res * 10) / 10;
-   dis.innerHTML="Your BMI is: "+bmi+" <br> <p style=color:"+col(bmi)+ "> "+rng(bmi)+"<br>"+shiftt(bmi,wt,ht)+"</p>";
+   dis.innerHTML="Your BMI is (আপনার বি.এম.আই): "+bmi+" <br> <p style=color:"+col(bmi)+ "> "+rng(bmi)+"<br>"+shiftt(bmi,wt,ht)+"</p>";
    bar.style.width=2*bmi+"%";
    bar.style.backgroundColor=col(bmi);
    tab.style.display="block";
@@ -109,18 +110,20 @@ let bmi=Math.round(res * 10) / 10;
 
 function shiftt(bmi,wt,ht)
 {
+	let lin=document.getElementById("link");
 	let nwt=0;
+	lin.style.opacity="1";
 	if(bmi<18.5)
 	{
 		nwt=Math.round((18.5*ht*ht-wt) * 10) / 10;
-		return "You have to gain "+nwt+" kG for normal BMI"
+		return "You have to gain "+nwt+" kG for normal BMI.<br> আপনাকে "+nwt+" কেজি ওজন বাড়াতে হবে ।"
 
 	}
 
 	else if(bmi>22.9)
 	{
        nwt=Math.round((wt-22.9*ht*ht) * 10) / 10;
-		return "You have to loose "+nwt+" kG for normal BMI"
+		return "You have to loose "+nwt+" kG for normal BMI.<br> আপনাকে "+nwt+" কেজি ওজন কমাতে হবে ।"
 	}
 	else
 
